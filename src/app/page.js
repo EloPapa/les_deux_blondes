65107ec59c63900da5e10263aed9e514a0d5ec26
EloPapa <ericbergeron2000@gmail.com>
2026-05-31 06:51:37 -0400

    t

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,22 @@
 
 import { useRef } from "react";
 import { useLanguage } from "../context/LanguageContext";
+
 import Header from "../components/Header";
+import data from "../data/lesDeuxBlondes.json";
 import Footer from "../components/Footer";
 
 export default function Home() {
   const { lang, t } = useLanguage();
-  const aboutRef = useRef(null);  // ← ajoute ça
+  const aboutRef = useRef(null);  
 
   const handleAboutScroll = () => {
-    aboutRef.current?.scrollIntoView({ behavior: "smooth" });  // ← et ça
+    aboutRef.current?.scrollIntoView({ behavior: "smooth" });  
   };
   const handlePresentationVideoScroll = () => {};
+
+  const aboutParagraphs =
+    lang === "fr" ? data.about_fr || data.about : data.about;
 
   return (
     <div className="relative flex flex-col min-h-screen">
@@ -32,7 +37,15 @@ export default function Home() {
           >
             {t.sections.about}
           </h1>
-        </div>
+        
+        <div className="text-xl lg:text-[1rem] xl:text-[0.952rem] 2xl:text-[1.9rem] max-w-2xl lg:max-w-2xl xl:max-w-4xl 2xl:max-w-5xl leading-relaxed xl:leading-loose 2xl:leading-loose">
+            {aboutParagraphs.map((paragraph, index) => (
+              <p key={index} className="mb-3 lg:mb-[1.05rem] xl:mb-[1.05rem] 2xl:mb-8">
+                {paragraph}
+              </p>
+            ))}
+          </div>        
+        </div>      
       </main>
 
       <Footer />
