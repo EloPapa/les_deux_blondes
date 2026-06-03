3328b8d45722d8eb895b4e69acfc8a901b65496f
EloPapa <ericbergeron2000@gmail.com>
2026-06-03 16:46:01 -0400

    t

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -180,8 +180,9 @@ export default function Content({ lang = "fr" }) {
           </div>
 
           {/* COLONNE DE DROITE */}
+          {/* Bar Jus */}
           <div style={{ display: "flex", flexDirection: "column", gap: "22px", alignItems: "stretch" }}>
-            <ContentCard src="/images/contenu/cat.png" alt={t.alt.imageContent3} ariaLabel={t.ariaLink} href={YOUTUBE_URL} external={true} cardStyle={{ width: "90%", aspectRatio: "4 / 4", alignSelf: "flex-start" }} />
+            <ContentCard src="/images/contenu/cat.png" alt={t.alt.imageContent3} ariaLabel={t.ariaLink} href={YOUTUBE_URL} external={true} cardStyle={{ width: "145%", aspectRatio: "1.4 / 1.8" , alignSelf: "flex-start" }} />
 
             {/* Tablette */}
             <div style={{ position: "relative", width: "100%" }}>
