6ffdd6daf3a62470c2e78b38b2447b094baecb97
EloPapa <ericbergeron2000@gmail.com>
2026-06-05 07:25:35 -0400

    t

--- src/app/page.js
+++ src/app/page.js
@@ -171,7 +171,7 @@ export default function Home() {
 
 
         {/* TITRE ABOUT EN FONT AMSTERDAM */}   
-        <div className="mt-15t lg:mt-[2.275rem] xl:mt-[2.275rem] pt-2 px-2" ref={aboutRef}>
+        <div className="mt-15t lg:mt-[2.275rem] xl:mt-[2.275rem] pt-2 px-2 flex flex-col items-center" ref={aboutRef}>
           <h1
             className="text-[1.5rem] mt-4 lg:text-[3rem] xl:text-[3rem] mb-[2.275rem] lg:mt-[2.275rem] lg:mb-[2.275rem] xl:mt-[2.275rem] xl:mb-[2.275rem]"
             style={{
