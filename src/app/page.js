4eb83b3fe1e4c29b2b0d5e29a21b1bf20ad2ae42
EloPapa <ericbergeron2000@gmail.com>
2026-06-05 08:28:12 -0400

    t

--- src/app/page.js
+++ src/app/page.js
@@ -170,15 +170,15 @@ export default function Home() {
         <div
           className="
             absolute z-10
-            right-[2%] top-[6%]
+            right-[2%] top-[10%]
             w-[120px] h-[120px]
-            sm:right-[2%] sm:top-[5%] sm:w-[180px] sm:h-[180px]
-            md:right-[2%] md:top-[4%] md:w-[250px] md:h-[250px]
-            lg:right-[2%] lg:top-[3%] lg:w-[243px] lg:h-[243px]
-            xl:right-[2%] xl:top-[3%] xl:w-[280px] xl:h-[280px]
-            2xl:right-[2%] 2xl:top-[3%] 2xl:w-[400px] 2xl:h-[400px]
+            sm:right-[2%] sm:top-[9%] sm:w-[180px] sm:h-[180px]
+            md:right-[2%] md:top-[8%] md:w-[250px] md:h-[250px]
+            lg:right-[2%] lg:top-[6%] lg:w-[243px] lg:h-[243px]
+            xl:right-[2%] xl:top-[6%] xl:w-[280px] xl:h-[280px]
+            2xl:right-[2%] 2xl:top-[6%] 2xl:w-[400px] 2xl:h-[400px]
             rounded-full overflow-hidden
-            "
+"
           >
           <img
             src="/images/contenu/avatar.png"
