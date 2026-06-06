6fe29d99c00e150cd36cb4b0498a2f2c887ef2b3
EloPapa <ericbergeron2000@gmail.com>
2026-06-06 19:39:54 -0400

    t

--- src/app/page.js
+++ src/app/page.js
@@ -16,8 +16,8 @@ export default function Home() {
 
   /* AJUSTER LE OFFSET QUAND ON SCROLL TO A PARTIR DU DES BOUTONS DU HEADER */
   const getHeaderOffset = () => {
-    if (window.innerWidth < 640) return 40;  
-    if (window.innerWidth < 1024) return 70; 
+    if (window.innerWidth < 640) return 10;  
+    if (window.innerWidth < 1024) return 40; 
     return 275;
   };
 
