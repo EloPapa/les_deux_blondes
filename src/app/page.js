135e33a31e13966cf30f89722979941d3cef2740
EloPapa <ericbergeron2000@gmail.com>
2026-06-06 20:02:29 -0400

    t

--- src/app/page.js
+++ src/app/page.js
@@ -17,8 +17,8 @@ export default function Home() {
   /* AJUSTER LE OFFSET QUAND ON SCROLL TO A PARTIR DU DES BOUTONS DU HEADER */
   const getHeaderOffset = () => {
     if (window.innerWidth < 640) return 10;   // Cellulaire et Tablette
-    if (window.innerWidth < 1024) return 25; // Écran Large
-    return 25; // Écran XL et plus
+    if (window.innerWidth < 1024) return 50; // Écran Large
+    return 50; // Écran XL et plus
   };
 
    {/* getBoundingClientRect().top donne la position relative au viewport actuel, donc en ajoutant window.scrollY tu obtiens la position absolue — et l'offset s'applique correctement dans les deux directions.*/}
