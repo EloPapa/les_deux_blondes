bc30e2cccbf666458ab0aebaf32bc211b61f40fb
EloPapa <ericbergeron2000@gmail.com>
2026-06-07 08:52:33 -0400

    t

--- src/app/page.js
+++ src/app/page.js
@@ -30,7 +30,7 @@ export default function Home() {
 
   const handleContentScroll = () => {
     const rect = contentRef.current.getBoundingClientRect();
-    const top = window.scrollY + rect.top + (60 * getHeaderOffset());
+    const top = window.scrollY + rect.top + (55 * getHeaderOffset());
     window.scrollTo({ top, behavior: "smooth" });
   };
 
