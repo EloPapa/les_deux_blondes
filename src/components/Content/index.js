9a5c7fd3f5ed48b11aa5072b71cfefb30fb42618
EloPapa <ericbergeron2000@gmail.com>
2026-06-07 11:58:48 -0400

    t

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -426,8 +426,8 @@ export default function Content({ lang = "fr" }) {
                 ariaLabel={t.ariaLink}
                 href="#"
                 external={false}
-                cardStyle={{ width: "100%", aspectRatio: "9 / 16" }}  {/* ← ratio ajusté */}
-                imgStyle={{ objectFit: "contain" }}                    {/* ← ajout */}
+                cardStyle={{ width: "100%", aspectRatio: "9 / 16" }}  
+                imgStyle={{ objectFit: "contain" }}                    
                 onClick={() => setShowVideoPopup(true)}
               />
 
